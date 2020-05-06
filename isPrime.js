13f13f691e38af35e8a3e93299f64e5b548da6a7
Jason <50801917+xasmodeanx@users.noreply.github.com>
2020-05-06 09:05:14 -0600

    Delete isPrime.js

--- isPrime.js
+++
@@ -1,9 +0,0 @@
-// Write a function called isPrime that returns a boolean based on whether the input number is prime or not.
-
-// ex.
-// const num = 6;
-// isPrime(num) = false;
-
-// ex
-// const num = 7;
-// isPrime(num) == true;
